f8b369ea8c26af3e28041155ce6461e18607125e
Robert <rquinones93@yahoo.com>
2019-03-03 11:42:52 -0800

    config change

--- config/config.js
+++ config/config.js
@@ -5,28 +5,16 @@
 require('dotenv').config();
 
 module.exports = {
-    development: {
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-    },
-    test: {
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-    },
-    production: {
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-    }
-  };
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  },
+  test: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  }
+};
